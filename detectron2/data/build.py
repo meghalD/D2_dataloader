f78197e5dc9c7560b824f1a0be818cbcb97d718b
meghalD <meghal.dani.er@gmail.com>
2021-07-23 15:44:51 +0530

    Update build.py

--- detectron2/data/build.py
+++ detectron2/data/build.py
@@ -345,10 +345,41 @@ def _train_loader_from_config(cfg, mapper=None, *, dataset=None, sampler=None):
 
 
 # TODO can allow dataset as an iterable or IterableDataset to make this function more general
-@configurable(from_config=_train_loader_from_config)
-def build_detection_train_loader(
-    dataset, *, mapper, sampler=None, total_batch_size, aspect_ratio_grouping=True, num_workers=0
+
+def build_detection_train_loader(cfg, mapper=None, *, dataset=None, sampler=None
 ):
+    
+    if dataset is None:
+        dataset = get_detection_dataset_dicts(
+            cfg.DATASETS.TRAIN,
+            filter_empty=cfg.DATALOADER.FILTER_EMPTY_ANNOTATIONS,
+            min_keypoints=cfg.MODEL.ROI_KEYPOINT_HEAD.MIN_KEYPOINTS_PER_IMAGE
+            if cfg.MODEL.KEYPOINT_ON
+            else 0,
+            proposal_files=cfg.DATASETS.PROPOSAL_FILES_TRAIN if cfg.MODEL.LOAD_PROPOSALS else None,
+        )
+        _log_api_usage("dataset." + cfg.DATASETS.TRAIN[0])
+
+    if mapper is None:
+        mapper = DatasetMapper(cfg, True)
+
+    if sampler is None:
+        sampler_name = cfg.DATALOADER.SAMPLER_TRAIN
+        logger = logging.getLogger(__name__)
+        logger.info("Using training sampler {}".format(sampler_name))
+        if sampler_name == "TrainingSampler":
+            sampler = TrainingSampler(len(dataset))
+        elif sampler_name == "RepeatFactorTrainingSampler":
+            repeat_factors = RepeatFactorTrainingSampler.repeat_factors_from_category_frequency(
+                dataset, cfg.DATALOADER.REPEAT_THRESHOLD
+            )
+            sampler = RepeatFactorTrainingSampler(repeat_factors)
+        else:
+            raise ValueError("Unknown training sampler: {}".format(sampler_name))
+    total_batch_size= cfg.SOLVER.IMS_PER_BATCH
+    aspect_ratio_grouping= cfg.DATALOADER.ASPECT_RATIO_GROUPING
+    num_workers= cfg.DATALOADER.NUM_WORKERS
+    
     """
     Build a dataloader for object detection with some default features.
     This interface is experimental.
@@ -383,6 +414,8 @@ def build_detection_train_loader(
     if sampler is None:
         sampler = TrainingSampler(len(dataset))
     assert isinstance(sampler, torch.utils.data.sampler.Sampler)
+    
+    
     return build_batch_data_loader(
         dataset,
         sampler,
